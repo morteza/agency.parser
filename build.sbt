8f4cd00b017b459ab136facecf549879abc53e9d
Morteza Ansarinia <ansarinia@me.com>
2017-09-22 19:57:45 +0330

    Fix latency, and a new script to extract RTs from WinEEG to compare them with PsychoPy results.

--- build.sbt
+++ build.sbt
@@ -6,6 +6,7 @@ scalaVersion := "2.12.3"
 
 //mainClass in (Compile, run)  := Some("app.HypnosisAppJsonParser")
 mainClass in (Compile, run)  := Some("eeg.EpochFixer")
+//mainClass in (Compile, run)  := Some("eeg.WinEEGReactionTimeExtractor")
 //mainClass in (Compile, run)  := Some("eeg.PsychoPySubjectCleaner")
 //mainClass in (Compile, run)  := Some("eeg.PsychoPyGroupCleaner")
 
--- src/main/scala/eeg/WinEEGReactionTimeExtractor.scala
+++ src/main/scala/eeg/WinEEGReactionTimeExtractor.scala
@@ -15,10 +15,10 @@ import scala.io.{Source => IOSource}
 /**
   * To fix WinEEG/PsychoPy labels, and prepare epoch files for EEGLAB
   */
-object EpochFixer extends App {
+object WinEEGReactionTimeExtractor extends App {
   val subject = Option(args(0))
 
-  args.foreach{subject => createSubjectEpochs(subject, System.getProperty("user.home")+"/Desktop/data")}
+  args.foreach{subject => extractRTForSubject(subject, System.getProperty("user.home")+"/Desktop/data")}
 
   /*
   val flow = Source(content.split("\n").toList) via
@@ -28,7 +28,7 @@ object EpochFixer extends App {
   flow.run()
   */
 
-  def createSubjectEpochs(subject: String, rootDir: String): Unit = {
+  def extractRTForSubject(subject: String, rootDir: String): Unit = {
     if (subject == null || subject.trim.length!=3)
       return
 
@@ -49,9 +49,11 @@ object EpochFixer extends App {
 
     var sb = StringBuilder.newBuilder
 
-    sb append "Latency Group Type Index\n"
+    sb append "Subject,Group,Order,RT\n"
 
     var i = 0
+
+    var rt = 0.0
 
     // drop header and practice rounds
     content.split("\n").drop(if (removePractice) 41 else 1).toList.foreach ( row => {
@@ -64,12 +66,17 @@ object EpochFixer extends App {
         codeMap.put(order, 0)
       else
         codeMap.put(order, codeMap.getOrElse(order, 0)+1)
-      if ("start".equalsIgnoreCase(eventType)) latency = latency + {if ((i<40) && !removePractice) 1.0 else 1.0} // move from start-of-fixation to start-of-stimulus
-      sb append s"$latency $group $eventType $order\n"
+      if ("start".equalsIgnoreCase(eventType)) {
+        latency = latency + {if ((i<40) && !removePractice) 1.2 else 1.2}
+        rt = latency
+      } else {
+        rt = latency - rt
+        sb append s"$subject,$group,$order,$rt\n"
+      }
       i += 1
     })
 
-    writeToFile(s"${rootDir}/${subject}/${subject}_eeg_epochs.txt", sb.mkString)
+    writeToFile(s"${rootDir}/${subject}/${subject}_wineeg_rt.csv", sb.mkString)
 
   }
 
