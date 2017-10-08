c5adeb48cb5f78934a7d3876dfa1c4240fb89341
Morteza Ansarinia <ansarinia@me.com>
2017-10-08 11:25:51 +0330

    Improve preprocessing, and ERP extraction.

--- matlab/preprocess_erp.m
+++ matlab/preprocess_erp.m
@@ -1,63 +1,105 @@
 % Agency ERP Generator Script - Oct 5, 2017
 % Morteza Ansarinia <ansarinia@me.com>
 % ------------------------------------------------
-clear all;
-subject = 'nsh';
-disp(['Processing ' subject '...'])
+
+% -------- Parameters ----------------------------
+study = 'erp_4bins';
+
+erpBaseline = [-50 0]; %'pre', 'whole', etc
+erpBaselineStr = '-50 0';
+%subject = input('Enter subject identifier: ','s');
 rootDataDir = '/Users/morteza/Desktop/data';
+figuresOutputDir = [rootDataDir '/misc/figures/' study];
+erpStorageDir = [rootDataDir '/misc/erp/studies/' study];
+preProcStorageDir = [rootDataDir '/misc/preproc/studies/' study];
 channelLocationFile = '/Users/morteza/Documents/MATLAB/eeglab14_1_1b/plugins/dipfit2.3/standard_BESA/standard-10-5-cap385.elp';
-binListerFile = [rootDataDir '/misc/erp/binlister.txt'];
-cd([rootDataDir '/' subject '/']);
-[ALLEEG EEG CURRENTSET ALLCOM] = eeglab; % ('no-gui');
-EEG = pop_fileio([rootDataDir '/' subject '/' subject '_raw.edf']);
-[ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 0,'setname',subject,'gui','off');
-EEG = eeg_checkset( EEG );
-EEG = pop_select( EEG,'nochannel',{'LABEL'});
-[ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 1,'savenew',subject,'overwrite','on','gui','off');
-EEG = eeg_checkset( EEG );
-EEG = pop_reref( EEG, 20);
-EEG=pop_chanedit(EEG, 'lookup',channelLocationFile);
-[ALLEEG EEG] = eeg_store(ALLEEG, EEG, CURRENTSET);
-EEG = eeg_checkset( EEG );
-EEG = pop_saveset( EEG, 'savemode','resave');
-[ALLEEG EEG] = eeg_store(ALLEEG, EEG, CURRENTSET);
-EEG = pop_eegfiltnew(EEG, [],0.5,1650,1,[],1);
-[ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 1,'setname',[subject '_filt'],'savenew',[subject '_filt'],'overwrite','on','gui','off');
-EEG = pop_eegfiltnew(EEG, [],20,110,0,[],1);
-[ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 1,'savenew',[subject '_filt'],'overwrite','on','gui','off');
-EEG = eeg_checkset( EEG );
-EEG = pop_interp(EEG, [11], 'spherical'); % C4
-EEG = eeg_checkset( EEG );
-EEG = pop_importevent( EEG, 'event',[rootDataDir '/' subject '/' subject '_erp_epochs.txt'],'fields',{'latency' 'type' 'etype' 'index'},'skipline',1,'timeunit',1,'optimalign','off');
-[ALLEEG EEG] = eeg_store(ALLEEG, EEG, CURRENTSET);
-EEG = eeg_checkset( EEG );
-EEG = pop_saveset( EEG, 'savemode','resave');
-[ALLEEG EEG] = eeg_store(ALLEEG, EEG, CURRENTSET);
-EEG  = pop_creabasiceventlist( EEG , 'AlphanumericCleaning', 'on', 'BoundaryNumeric', { -99 }, 'BoundaryString', { 'boundary' } );
-[ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 1,'savenew',[subject '_filt_elist'],'overwrite','on','gui','off');
-EEG = eeg_checkset( EEG );
-EEG  = pop_binlister( EEG , 'BDF', binListerFile, 'IndexEL',  1, 'SendEL2', 'Workspace&EEG', 'UpdateEEG', 'on', 'Voutput', 'EEG' );
-[ALLEEG EEG] = eeg_store(ALLEEG, EEG, CURRENTSET);
-EEG = eeg_checkset( EEG );
-EEG  = pop_overwritevent( EEG, 'binlabel');
-[ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 1,'savenew',[subject '_filt_elist_bins'],'overwrite','on','gui','off');
-EEG = eeg_checkset( EEG );
-EEG = pop_epochbin( EEG , [-200.0  800.0],  'pre');
-[ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 1,'savenew',[subject '_filt_elist_bins_be'],'overwrite','on','gui','off');
 
-[EEG.icaweights, EEG.icasphere] = binica(EEG.data, 'extended', 1);
-EEG.incaact = EEG.icaweights*EEG.icasphere*EEG.data;
-EEG = eeg_checkset(EEG, 'ica');
-EEG = pop_saveset( EEG, 'savemode','resave');
+binListerFile = [rootDataDir '/misc/erp/binlister_4bins.txt'];
 
-EEG  = pop_artmwppth( EEG , 'Channel',  1:19, 'Flag', [ 1 2], 'Threshold',  150, 'Twindow', [ -100 500], 'Windowsize',  200, 'Windowstep',  50 );
-EEG  = pop_artblink( EEG , 'Blinkwidth',  300, 'Channel',  1:19, 'Crosscov',  0.7, 'Flag', [ 1 3], 'Twindow', [ -100 696] );
-[ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 1,'savenew',[subject '_filt_elist_bins_be_ar_blink'],'overwrite','on','gui','off');
+subjects = {'nsh', 'aka', 'ach', 'akh', 'bah', 'fhe', 'mhe', 'mkh', 'nkh', 'rho', 'rsa', 'sa1', 'sa2', 'sfa', 'sja'};
+%--------------------------------------------------
 
-ERP = pop_averager( EEG , 'Criterion', 'good', 'ExcludeBoundary', 'on', 'SEM', 'on' );
-ERP = pop_savemyerp(ERP, 'erpname', [subject '_erp'], 'filename', [subject '_erp.erp'], 'filepath', [rootDataDir '/' subject], 'Warning', 'on');
+mkdir(figuresOutputDir)
+mkdir(erpStorageDir)
+mkdir(preProcStorageDir)
 
-% Plot ERP for Fz, Cz, and Pz
-ERP = pop_ploterps( ERP,  1:3,  5:5:15 , 'AutoYlim', 'on', 'Axsize', [ 0.05 0.08], 'BinNum', 'on', 'Blc', 'pre', 'Box', [ 3 1], 'ChLabel','on', 'FontSizeChan',  10, 'FontSizeLeg',  12, 'FontSizeTicks',  10, 'LegPos', 'bottom', 'Linespec', {'k-' , 'r-' , 'b-' }, 'LineWidth',  1,'Maximize', 'on', 'Position', [ 93.7143 15.0714 106.857 31.9286], 'Style', 'Classic', 'Tag', 'ERP_figure', 'Transparency',  0, 'xscale',[ -300.0 800.0   -200:200:800 ], 'YDir', 'normal' );
 
-eeglab redraw;
+numOfSubjects = length(subjects)
+
+eeglab;
+
+for sIndex = 1:numOfSubjects
+  subject = subjects{sIndex};
+
+  disp(['Processing ' subject '...']);
+  cd([rootDataDir '/' subject '/']);
+
+  % Open RAW data using FILEIO
+  EEG = pop_fileio([rootDataDir '/' subject '/' subject '_raw.edf']);
+
+  % Select data (19 channels)
+  EEG = pop_select( EEG,'nochannel',{'LABEL','AA'});
+
+  % Load channel locations
+  EEG=pop_chanedit(EEG, 'lookup',channelLocationFile);
+  EEG = eeg_checkset( EEG );
+
+  % Interpolate noisy electrods
+  EEG = pop_interp(EEG, [11], 'spherical'); % C4
+  EEG = eeg_checkset( EEG );
+
+  % Rereference to average
+  %EEG = pop_reref( EEG, 20);
+  EEG = pop_reref( EEG, []);
+
+  % Filter (2-30Hz)
+  EEG = pop_eegfiltnew(EEG, [],2,1650,1,[],0);
+  EEG = pop_eegfiltnew(EEG, [],30,110,0,[],0);
+  EEG = eeg_checkset( EEG );
+
+  % Import epochs
+  EEG = pop_importevent( EEG, 'event',[rootDataDir '/' subject '/' subject '_erp_epochs.txt'],'fields',{'latency' 'type' 'etype' 'index'},'skipline',1,'timeunit',1,'optimalign','off');
+
+  % Create basic EVENTLIST (ERPLAB)
+  EEG  = pop_creabasiceventlist( EEG , 'AlphanumericCleaning', 'on', 'BoundaryNumeric', { -99 }, 'BoundaryString', { 'boundary' } );
+  EEG = eeg_checkset( EEG );
+
+  % Create bins using BINLISTER
+  EEG  = pop_binlister( EEG , 'BDF', binListerFile, 'IndexEL',  1, 'SendEL2', 'Workspace&EEG', 'UpdateEEG', 'on', 'Voutput', 'EEG' );
+  EEG = eeg_checkset( EEG );
+
+  % Update EEG epoch labels
+  EEG  = pop_overwritevent( EEG, 'binlabel');
+  EEG = eeg_checkset( EEG );
+
+  EEG = pop_editset(EEG, 'setname', [subject '_preproc']);
+
+  % Extract epochs
+  EEG = pop_epochbin(EEG ,[-200.0 800.0],  erpBaseline);
+  EEG = pop_saveset( EEG, 'filename', [subject '_preproc.set'], 'filepath', preProcStorageDir);
+
+  % ICA
+  %[EEG.icaweights, EEG.icasphere] = binica(EEG.data, 'extended', 1);
+  % %EEG.incaact = EEG.icaweights*EEG.icasphere*EEG.data;
+  %EEG = eeg_checkset(EEG, 'ica');
+  %EEG = pop_saveset( EEG, 'savemode','resave');
+
+  % Artifact detection (Peak to peak, and blinks) for -100,500 interval
+  EEG  = pop_artmwppth( EEG , 'Channel',  1:19, 'Flag', [ 1 2], 'Threshold',  150, 'Twindow', [-100 500], 'Windowsize',  200, 'Windowstep',  50 );
+  EEG  = pop_artblink( EEG , 'Blinkwidth',  400, 'Channel',  1:19, 'Crosscov',  0.7, 'Flag', [ 1 3], 'Twindow', [ -100 500] );
+
+  % Compute ERPs + new diff bins
+  ERP = pop_averager( EEG , 'Criterion', 'good', 'ExcludeBoundary', 'on', 'SEM', 'on' );
+  ERP = pop_binoperator( ERP, {  'bin5 = bin1 - bin4',  'bin6 = bin2 - bin4', 'bin7 = bin3 - bin4'});
+  ERP = pop_savemyerp(ERP, 'erpname', [subject '_' study '_erp'], 'filename', [subject '_erp_' study '.erp'], 'filepath', erpStorageDir, 'Warning', 'on');
+
+  % Plot ERP for Fz, Cz, and Pz
+  ERP = pop_ploterps( ERP,  1:4,  5:5:15 , 'AutoYlim', 'on', 'Axsize', [0.05 0.08], 'BinNum', 'on', 'Blc', erpBaselineStr, 'Box', [4 1], 'ChLabel', 'on', 'FontSizeChan',  10, 'FontSizeLeg',  12, 'FontSizeTicks',  10, 'LegPos', 'bottom', 'Linespec', {'r-' , 'k-' , 'b-' , 'r:', 'k:' }, 'LineWidth', 1, 'Maximize', 'on', 'Style', 'Classic', 'Tag', ['FzCzPz_' subject '_' study], 'Transparency',  0, 'xscale', [ -100.0 600.0   -100:50:600 ], 'YDir', 'normal' );
+  ERP = pop_exporterplabfigure( ERP , 'Filepath', figuresOutputDir, 'Format', 'pdf', 'Resolution',  300, 'SaveMode', 'auto', 'Tag', {'ERP_figure' ['FzCzPz_' subject '_' study] } );
+
+  eeglab redraw;
+  erplab redraw;
+end
+
+% for time I forget to do so...
+cd('Users/morteza/workspace/agency.parser/matlab/')
---
+++ src/main/scala/eeg/ERPActionEpochFileGenerator.scala
@@ -0,0 +1,107 @@
+package eeg
+
+import java.nio.charset.StandardCharsets
+import java.nio.file.{Files, Paths}
+import java.util.Scanner
+
+import scala.collection.mutable
+import scala.io.{Source => IOSource}
+
+/**
+  * To fix WinEEG/PsychoPy labels, and prepare epoch files for ERPLAB
+  */
+object ERPActionEpochFileGenerator extends App {
+
+  val subjects = Map (
+    "ach" -> "nonhyp",
+    "aka" -> "hyp",
+    "akh" -> "hyp",
+    "bah" -> "nonhyp",
+    "fhe" -> "hyp",
+    "mhe" -> "nonhyp",
+    "mkh" -> "hyp",
+    "nkh" -> "hyp",
+    "nsh" -> "hyp",
+    "rho" -> "hyp",
+    "rsa" -> "hyp",
+    "sa1" -> "hyp",
+    "sa2" -> "hyp",
+    "sfa" -> "hyp",
+    "sja" -> "hyp",
+  )
+
+  subjects.foreach{s => createSubjectEpochs(s._1, System.getProperty("user.home")+"/Desktop/data")}
+
+  def createSubjectEpochs(subject: String, rootDir: String): Unit = {
+    if (subject == null || subject.trim.length!=3)
+      return
+
+    println(s"Generating ERP epoch file for $subject")
+    val removePractice = true
+
+    val path = Option(s"${rootDir}/${subject}/${subject}_labels.txt")
+    val trialsPath = Option(s"${rootDir}/${subject}/${subject}_erp_trial_orders.csv")
+
+    val content = loadFile(path.get)
+    val trialsContent = loadFile(trialsPath.get)
+
+    val trials = trialsContent.split("\n").drop(if (removePractice) 41 else 1) // drop header and practice rounds
+
+    //implicit val system = ActorSystem("EpochFixerSystem")
+    //implicit val materializer = ActorMaterializer()
+
+    var codeMap = mutable.Map[Int, Int]()
+
+    var sb = StringBuilder.newBuilder
+
+    sb append "Latency Group Type Index\n"
+
+    var i = 0
+    var startTimestamp = 0.0
+    var timeouts = 0
+
+    // drop header and practice rounds
+    content.split("\n").drop(if (removePractice) 41 else 1).toList.foreach ( row => {
+      val s = new Scanner(row)
+      var latency = s.nextDouble
+      val order = s.nextInt
+      //val group = trials(Math.floor(i / 2).toInt)
+      var group = trials(i)
+      val eventType = if (codeMap.getOrElse(order, 0)==0) "start" else "rt"
+
+      if (codeMap.getOrElse(order, 0) >= 1)
+        codeMap.put(order, 0)
+      else
+        codeMap.put(order, codeMap.getOrElse(order, 0)+1)
+
+      if ("start".equalsIgnoreCase(eventType)) {
+        latency = latency + 1.2 // add fixation, jitter, and averaged USB delay
+        startTimestamp = latency
+      } //{if ((i<40) && !removePractice) 1.2 else 1.2} // move from start-of-fixation to start-of-stimulus
+      if ("rt".equalsIgnoreCase(eventType)) {
+        //println(group)
+        var rt = group.split(":")(1).toDouble
+        latency = rt + startTimestamp + {if (rt<3.9) 0.2 else 0.0} // to compensate jitter and delayed keyboard sensitivity
+        group = group.split(":")(0)
+        if (group==9 || group==12) timeouts+=1
+        Seq(rt).filter(rt => rt>3.5 || rt<0.1).map{_ => group="12"; timeouts+=1} // timeout groups if rt>=3.5
+      }
+      sb append f"$latency%1.3f $group $eventType $order%1d\n"
+      i += 1
+    })
+
+    println(s"$timeouts trials marked as timeout (code 12).")
+
+    writeToFile(s"${rootDir}/misc/erp_epochs/${subject}_erp_action_epochs.txt", sb.mkString)
+    writeToFile(s"${rootDir}/${subject}/${subject}_erp_action_epochs.txt", sb.mkString)
+
+  }
+
+  def loadFile(path: String): String = {
+    IOSource.fromFile(path, "UTF-8").mkString
+  }
+
+  def writeToFile(path: String, str: String) = {
+    Files.write(Paths.get(path), str.getBytes(StandardCharsets.UTF_8))
+  }
+}
