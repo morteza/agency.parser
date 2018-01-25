d2bfe05f316cb9e1dda2df0706ef9c0072f926b6
Morteza Ansarinia <ansarinia@me.com>
2018-01-25 19:41:44 +0330

    Update MATLAB preprocessing script for SIFT connectivity analysis...

--- matlab/preprocess_for_sift.m
+++ matlab/preprocess_for_sift.m
@@ -9,9 +9,7 @@ filter_low = 1.0;
 rootDataDir = '/Users/morteza/Desktop/sift_data';
 preProcDir = [rootDataDir '/preproc/'];
 channelLocationFile = '/Users/morteza/Documents/MATLAB/eeglab14_1_1b/plugins/dipfit2.3/standard_BESA/standard-10-5-cap385.elp';
-%subjects = {'nsh', 'aka', 'ach', 'akh', 'bah', 'fhe', 'mhe', 'mkh', 'nkh', 'rho', 'rsa', 'sa1', 'sa2', 'sfa', 'sja'};
-subjects = {'nsh'};
-
+subjects = {'nsh', 'aka', 'ach', 'akh', 'bah', 'fhe', 'mhe', 'mkh', 'nkh', 'rho', 'rsa', 'sa1', 'sa2', 'sfa', 'sja'};
 %--------------------------------------------------
 
 
@@ -19,7 +17,7 @@ disp('Preprocessing...');
 
 numOfSubjects = length(subjects);
 
-eeglab;
+eeglab 'nogui';
 
 for sIndex = 1:numOfSubjects
   subject = subjects{sIndex};
@@ -31,39 +29,56 @@ for sIndex = 1:numOfSubjects
 
   % Open RAW data using FILEIO
   EEG = pop_fileio([rootDataDir '/raw/' subject '_raw.edf']);
+  EEG.setname = subject;
 
-  % Select data (19 channels)
-  EEG = pop_select( EEG,'nochannel',{'AA'});
+  % Select channels (19 channels + AA)
+  EEG = pop_select( EEG,'nochannel',{'LABEL'});
 
-  % Load channel locations
-  EEG=pop_chanedit(EEG, 'lookup',channelLocationFile);
-  EEG = eeg_checkset(EEG);
-
-  % Interpolate noisy electrodes (C4)
-  EEG = pop_interp(EEG, [11], 'spherical'); % C4
-  EEG = eeg_checkset(EEG);
-
-  % Rereference to average (AA=20)
-  EEG = pop_reref( EEG, 20);
+  % Rereference to AA (ch. 20)
+  EEG = pop_reref(EEG, 20);
+  % Rereference to average
   EEG = pop_reref(EEG, []);
 
-  % Filter
-  EEG = pop_eegfiltnew(EEG, [], filter_high,110 ,0,[],0);
-  EEG = pop_eegfiltnew(EEG, [], filter_low ,8250,1,[],0);
-  EEG = eeg_checkset( EEG );
+  % Import events
+  EEG = pop_importevent(EEG, 'event',[rootDataDir '/events/' subject '_epochs_correct_trials.txt'],'fields',{'latency' 'type' 'etype' 'index'},'skipline',1,'timeunit',1,'optimalign','off');
+ 
+  % Load channel locations
+  EEG=pop_chanedit(EEG, 'lookup', channelLocationFile);
+  EEG = eeg_checkset(EEG);
 
-  % Import epochs
-  EEG = pop_importevent( EEG, 'event',[rootDataDir '/events/' subject '_erp_epochs.txt'],'fields',{'latency' 'type' 'etype' 'index'},'skipline',1,'timeunit',1,'optimalign','off');
+  % Interpolate noisy electrodes (C4 is channel 11 and was noisy due to the problematic electrode)
+  EEG = pop_interp(EEG, 11, 'spherical');
+  EEG = eeg_checkset(EEG);
+
+  % Remove line noise using CleanLine on all channels
+  EEG = pop_cleanline(EEG, 'bandwidth', 2,'chanlist', [1:19], 'computepower', 0, 'linefreqs', [50 100 150 200 250],...
+        'normSpectrum', 0, 'p', 0.01, 'pad', 2, 'plotfigures', 0, 'scanforlines', 1, 'sigtype', 'Channels', 'tau', 100,...
+        'verb', 1, 'winsize', 4, 'winstep', 4);
+    
+  % Filters
+  EEG = pop_eegfiltnew(EEG, [], filter_low, 1650, 1, [], 0);
+  EEG = pop_eegfiltnew(EEG, [], filter_high, 110, 0, [], 0);
+  EEG = eeg_checkset(EEG);
 
   % ICA (binica)
+  %dataRank = rank(double(EEG.data'));
   [EEG.icaweights, EEG.icasphere] = binica(EEG.data, 'extended', 1);
   EEG.incaact = EEG.icaweights*EEG.icasphere*EEG.data;
-  %EEG = eeg_checkset(EEG, 'ica');
+  EEG = eeg_checkset(EEG, 'ica');
 
+  % Extract epochs and rebase with the following parameters
+  %   (baseline -100 to 0, and epochs -1000 to 2000 in millis)
+  %TODO: manually run these commands after removing the blink ICA comp
+  %EEG = pop_epoch(EEG, {'expl' 'free' 'impl'}, [-1  2], 'epochinfo', 'yes');
+  %EEG = eeg_checkset(EEG);
+  %EEG = pop_rmbase(EEG, [-100 0]);
+  %EEG = eeg_checkset(EEG);
+  
   % Save dataset
-  EEG = pop_editset(EEG, 'setname',  [subject '_preproc_ica']);
-  EEG = pop_saveset(EEG, 'filename', [subject '_preproc_ica.set'], 'filepath', preProcDir);
+  EEG = pop_editset(EEG, 'setname',  [subject '_preproc_ica_epochs']);
+  EEG = pop_saveset(EEG, 'filename', [subject '_preproc_ica_epochs.set'], 'filepath', preProcDir);
   eeglab redraw;
+  
 end
 
 % for time I forget to do so...
