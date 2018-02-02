815b7337fc57c6309ff9cdd00e9058216ce25bf4
Morteza Ansarinia <ansarinia@me.com>
2018-02-02 19:33:45 +0330

    Datasets are now generated for each conditions instead of subjects.

--- matlab/preprocess_for_sift.m
+++ matlab/preprocess_for_sift.m
@@ -3,15 +3,15 @@
 % ------------------------------------------------
 
 % -------- Parameters ----------------------------
-
-filter_high = 30;
-filter_low = 1.0;
+erp_range = [-0.2 1];
+erp_baseline = [-100 0];
+filter_high = 45;
+filter_low = 0.5;
 rootDataDir = '/Users/morteza/Desktop/sift_data';
 preProcDir = [rootDataDir '/preproc/'];
 channelLocationFile = '/Users/morteza/Documents/MATLAB/eeglab14_1_1b/plugins/dipfit2.3/standard_BESA/standard-10-5-cap385.elp';
 subjects = {'nsh', 'aka', 'ach', 'akh', 'bah', 'fhe', 'mhe', 'mkh', 'nkh', 'rho', 'rsa', 'sa1', 'sa2', 'sfa', 'sja'};
 %--------------------------------------------------
-
 
 disp('Preprocessing...');
 
@@ -34,17 +34,17 @@ for sIndex = 1:numOfSubjects
   % Select channels (19 channels + AA)
   EEG = pop_select( EEG,'nochannel',{'LABEL'});
 
-  % Rereference to AA (ch. 20)
-  EEG = pop_reref(EEG, 20);
-  % Rereference to average
-  EEG = pop_reref(EEG, []);
-
   % Import events
   EEG = pop_importevent(EEG, 'event',[rootDataDir '/events/' subject '_epochs_correct_trials.txt'],'fields',{'latency' 'type' 'etype' 'index'},'skipline',1,'timeunit',1,'optimalign','off');
  
   % Load channel locations
   EEG=pop_chanedit(EEG, 'lookup', channelLocationFile);
   EEG = eeg_checkset(EEG);
+
+  % Rereference to AA (ch. 20)
+  EEG = pop_reref(EEG, 20);
+  % Rereference to average
+  EEG = pop_reref(EEG, []);
 
   % Interpolate noisy electrodes (C4 is channel 11 and was noisy due to the problematic electrode)
   EEG = pop_interp(EEG, 11, 'spherical');
@@ -56,8 +56,8 @@ for sIndex = 1:numOfSubjects
         'verb', 1, 'winsize', 4, 'winstep', 4);
     
   % Filters
-  EEG = pop_eegfiltnew(EEG, [], filter_low, 1650, 1, [], 0);
-  EEG = pop_eegfiltnew(EEG, [], filter_high, 110, 0, [], 0);
+  EEG = pop_eegfiltnew(EEG, [], filter_low, [], 1, [], 0);
+  % EEG = pop_eegfiltnew(EEG, [], filter_high, [], 0, [], 0);
   EEG = eeg_checkset(EEG);
 
   % ICA (binica)
@@ -69,15 +69,43 @@ for sIndex = 1:numOfSubjects
   % Extract epochs and rebase with the following parameters
   %   (baseline -100 to 0, and epochs -1000 to 2000 in millis)
   %TODO: manually run these commands after removing the blink ICA comp
-  %EEG = pop_epoch(EEG, {'expl' 'free' 'impl'}, [-1  2], 'epochinfo', 'yes');
-  %EEG = eeg_checkset(EEG);
-  %EEG = pop_rmbase(EEG, [-100 0]);
-  %EEG = eeg_checkset(EEG);
+  EEG_expl = pop_epoch(EEG, {'expl'}, erp_range, 'epochinfo', 'yes');
+  EEG_expl = eeg_checkset(EEG_expl);
+  EEG_expl = pop_rmbase(EEG_expl, erp_baseline);
+  EEG_expl = eeg_checkset(EEG_expl);
+  EEG_expl = pop_editset(EEG_expl, 'setname',  [subject '_preproc_ica_expl']);
+  EEG_expl = pop_saveset(EEG_expl, 'filename', [subject '_preproc_ica_expl.set'], 'filepath', preProcDir);
+
+  % Implicit
+  EEG_impl = pop_epoch(EEG, {'impl'}, erp_range, 'epochinfo', 'yes');
+  EEG_impl = eeg_checkset(EEG_impl);
+  EEG_impl = pop_rmbase(EEG_impl, erp_baseline);
+  EEG_impl = eeg_checkset(EEG_impl);
+  EEG_impl = pop_editset(EEG_impl, 'setname',  [subject '_preproc_ica_impl']);
+  EEG_impl = pop_saveset(EEG_impl, 'filename', [subject '_preproc_ica_impl.set'], 'filepath', preProcDir);
   
-  % Save dataset
+  % Free
+  EEG_free = pop_epoch(EEG, {'free'}, erp_range, 'epochinfo', 'yes');
+  EEG_free = eeg_checkset(EEG_free);
+  EEG_free = pop_rmbase(EEG_free, erp_baseline);
+  EEG_free = eeg_checkset(EEG_free);
+  EEG_free = pop_editset(EEG_free, 'setname',  [subject '_preproc_ica_free']);
+  EEG_free = pop_saveset(EEG_free, 'filename', [subject '_preproc_ica_free.set'], 'filepath', preProcDir);
+
+  % Save the main dataset
   EEG = pop_editset(EEG, 'setname',  [subject '_preproc_ica_epochs']);
   EEG = pop_saveset(EEG, 'filename', [subject '_preproc_ica_epochs.set'], 'filepath', preProcDir);
+  
   eeglab redraw;
+  
+  % Reject ICA components using ADJUST extension
+  %cd(preProcDir);
+  %pop_ADJUST_interface([subject '_preproc_ica_epochs.set']);
+  %EEG = eeg_checkset(EEG);
+  
+  % Remove rejected components
+  %EEG = pop_subcomp(EEG, [%comps%], 0);
+  %EEG = eeg_checkset(EEG);
   
 end
 
